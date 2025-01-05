1b8dcbb7b7d568d8dcc336bb1733b637ec569a07
devboell <devboell@gmail.com>
2025-01-05 08:17:08 +0100

    install neo4j

---
+++ app/data/neo4j-session.ts
@@ -0,0 +1,9 @@
+import neo4j from 'neo4j-driver'
+
+const driver = neo4j.driver(
+  'bolt://localhost:7687',
+  neo4j.auth.basic('neo4j', 'history-app')
+)
+const session = driver.session({ database: 'history-dev' })
+
+export default session
---
+++ app/routes/persons.tsx
@@ -0,0 +1,37 @@
+// import type { Route } from './+types/home'
+import session from '../data/neo4j-session'
+import type { Record } from 'neo4j-driver'
+
+export function meta() {
+  return [
+    { title: 'Persons' },
+    { name: 'description', content: 'View Persons' },
+  ]
+}
+
+type Person = {
+  id: string
+  name: string
+}
+export async function loader(/*{}  params  : Route.LoaderArgs*/) {
+  const data = await session.run('MATCH (p:Person) RETURN p')
+  const persons = data.records.map(
+    (record: Record) => record.get('p').properties
+  )
+  return persons
+}
+
+export default function Persons({ loaderData }: { loaderData: Person[] }) {
+  console.log('loaderData', loaderData)
+
+  if (!loaderData) {
+    return <div>Loading...</div>
+  }
+  return (
+    <div>
+      {loaderData.map((person: Person) => (
+        <div key={`person-${person.id}`}>{person.name}</div>
+      ))}
+    </div>
+  )
+}
